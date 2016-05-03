8f2b3a9cafa12747a463555518344086a7de516f
Wouter Roosendaal <tuvokki@gmail.com>
2016-05-03 17:10:37 +0200

    ELO penalty

--- imports/components/players/players.js
+++ imports/components/players/players.js
@@ -5,8 +5,10 @@ import { Players } from '/imports/api/players.js';
 import template from './players.html';
 
 class PlayersCtrl {
-  constructor($scope) {
+  constructor($scope, $ionicPopup, $ionicListDelegate) {
     $scope.viewModel(this);
+    this.$ionicPopup = $ionicPopup;
+    this.$ionicListDelegate = $ionicListDelegate;
     
     // TODO: figure out which publications are needed here
     this.subscribe('players');
@@ -15,12 +17,41 @@ class PlayersCtrl {
 
     console.log('in players controller');
 
+    this.showPlayerInfo = 0;
+    
     this.helpers({
       data() {
         return Players.find();
       }
     });
 
+  }
+  
+  toggleInfo(player) {
+    if (this.showPlayerInfo == player._id) {
+      this.showPlayerInfo = 0;
+    } else {
+      this.showPlayerInfo = player._id;
+    }
+  }
+
+  eloPenalty(player) {
+    let confirmPopup = this.$ionicPopup.confirm({
+      title: 'Punish ' + player.name,
+      template: 'This will substract 100 ELO from the total of this player.<br><b>Do you want to punish '+player.name+'?</b>'
+    });
+
+    confirmPopup.then((res) => {
+      if (res) {
+        Players.update({ _id: player._id },
+        {
+          $inc: { elo: -100 } //decrese the elo by 100
+        })
+      } else {
+        return;
+      }
+      this.$ionicListDelegate.closeOptionButtons();
+    });
   }
 
 }
@@ -30,7 +61,7 @@ export default angular.module('players', [
 ])
   .component('players', {
     templateUrl: 'imports/components/players/players.html',
-    controller: ['$scope', PlayersCtrl],
+    controller: ['$scope', '$ionicPopup', '$ionicListDelegate', PlayersCtrl],
     controllerAs: 'players'
   })
   .config(($stateProvider) => {
